6e455b0b288de9c29fc023bc0050891231838a9f
Grigiryitstud <initstudentitin@gmail.com>
2022-10-16 18:08:53 +0300

    feat: some done (Sun, Oct 16, 2022 06:08:52 PM)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,47 +1,171 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rot = null;
+  }
+  
 
+  add(value) {    
+    if(value) {    
+      this.rot = addWithin(this.rot, value);
+      function addWithin(node, value) {
+        if(!node) {
+          return new Node(value);
+        }
+
+        if(node.value === value) {
+          return node;
+        }
+        if(value < node.value) {
+          node.left = addWithin(node.left, value);       
+        } else {
+          node.right = addWithin(node.right, value);
+        }
+        return node;
+      }
+    }
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(value) {
+    return searchWithin(this.rot, value);
+    function searchWithin(node, value) {
+      if (!node) {
+        return false;
+      }
+      if (node.value === value) {
+        return true;
+      }
+      return value < node.value ?
+      searchWithin(node.left, value) :
+      searchWithin(node.right, value);
+    }
+  }
+  // leftTraverse(cb, value) {
+  //   doLeft(this.rot, cb);
+  //   function doLeft(node, cb) {
+  //     if(node.value === value) {
+  //       return node
+  //     } else
+  //     if (node) {
+  //       doLeft(node.left,cb);
+  //       cb(node.value);
+  //       doLeft(node.right, cb);
+  //     }
+  //   }
+  // }
+  // rightTraverse(cb) {
+  //   doRight(this.rot, cb);
+  //   function doRight(node, cb) {
+  //     if (node) {
+  //       doRight(node.right,cb);
+  //       cb(node.value);
+  //       doRight(node.left, cb);
+  //     }
+  //   }
+  // }
+
+  find(value) {
+    return searchWithin(this.rot, value);
+    function searchWithin(node, value) {
+      if (!node) {
+        return null;
+      }
+      if (node.value === value) {
+        return node;
+      }
+      return value < node.value ?
+      searchWithin(node.left, value) :
+      searchWithin(node.right, value);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(value) {
+    this.rot = removeNode(this.rot, value);
+    function removeNode(node,value) {
+      if(!node) {
+        return null;
+      }
+      if (value < node.value) {
+        node.left = removeNode(node.left, value);
+        return node;
+      } else if (node.value < value) {
+        node.right = removeNode(node.right, value);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+        if(!node.left) {
+          node = node.right;
+          return node;
+        }
+        if(!node.right) {
+          node = node.left;
+          return node;
+        }
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+        node.value = minFromRight.value;
+        node.right = removeNode(node.right, minFromRight.value);
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rot) {
+      return
+    }
+    let node = this.rot;
+    while (node.left) {
+      node = node.left;
+    }
+    return node.value;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rot) {
+      return
+    }
+    let node = this.rot;
+    while (node.right) {
+      node = node.right;
+    }
+    return node.value;
   }
+  leftTraverse(cb) {
+    doLeft(this.rot, cb);
+    function doLeft(node, cb) {
+      if (node) {
+        doLeft(node.left,cb);
+        cb(node.value);
+        doLeft(node.right, cb);
+      }
+    }
+  }
+  rightTraverse(cb) {
+    doRight(this.rot, cb);
+    function doRight(node, cb) {
+      if (node) {
+        doRight(node.right,cb);
+        cb(node.value);
+        doRight(node.left, cb);
+      }
+    }
+  }
+
 }
 
 module.exports = {
--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    let main = this.head;
+      while(main.next) {
+        alert(main.value)
+        main = main.next;
+      }
+  }
+
+  enqueue(x) {
+    if (this.length == 0) {
+      this.head = new ListNode(x);
+    } else {
+      let main = this.head;
+      while(main.next) {
+        main = main.next;
+      }
+      main.next = new ListNode(x)
+    }
+    this.length ++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let main = this.head;
+    this.head = main.next
   }
 }
 
